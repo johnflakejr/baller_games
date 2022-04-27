1650b3162e667ff059c57df7f0e4576d744a333d
John F. Lake Jr <john.f.lake.jr@gmail.com>
2022-04-26 21:58:04 -0400

    Pixi.js in react.  Need to move my initial missile command stuff over

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './main.css';
+import {ElijahMissileCommand} from './EMC';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div id="explanation">
+      <h1 className="writeup">You should pick me for the listener league.</h1>
+      <div className="writeup">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Am I great at fantasy football?  No. In fact:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ul>
+          <li>
+            I've lost to my two year-old in both years he's played in our family league. 
+          </li>
+          <li>
+            I traded AWAY Cooper Kupp last year for Dalvin Cook in one league.  
+          </li>
+        </ul> 
+        <p>
+          Unlucky? Maybe? Foolish?  Absolutely. 
+        </p>
+        <p>
+          You don't want to read a ton of text, so let me just say this: I love fantasy football
+          and I love the show.  My wife introduced me to your podcast a few years ago and I've been 
+          a weekly listener ever since. I would love to be a part of the listener league.  Enjoy the
+          games I've created for you in exchange for considering me for a spot!!!
+        </p>
+      </div>
+      </div>
+      <ElijahMissileCommand/>
     </div>
   );
 }
